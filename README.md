12b549f6babf216ee1a8c1aaf14d48128cf97126
MrM1y4g1 <3052816+MrM1y4g1@users.noreply.github.com>
2023-09-20 10:57:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# MyPowershellSnippets
+# My PowerShell Snippets
+
+## Description
+
+This repository is a collection of various PowerShell snippets, functions, and scripts that aim to make life easier for sysadmins, developers, and automation enthusiasts. These snippets cover a wide range of tasks and are free for anyone to use.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. Clone this repository to your local machine.
+```bash
+git clone https://github.com/MrM1y4g1/MyPowershellSnippets.git
+```
+2. Navigate to the directory.
+```bash
+cd MyPowershellSnippets
+```
+
+## Usage
+
+Each snippet or script in this repository comes with its own set of instructions and examples, usually provided in comments at the top of the file. To use a snippet, simply copy the code into your PowerShell session or script, or run the script directly.
+
+For example, to run a script from this repository:
+```powershell
+.\path\to\script.ps1 -Parameter value
+```
+
+## Contributing
+
+Contributions are welcome! If you have a snippet or script that you think would be a valuable addition to this collection, please feel free to open a pull request. Make sure to include comments in your code explaining what it does, how to use it, and any dependencies it may have.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ functions/Get-ObjectWithMatchingProperty.ps1
@@ -0,0 +1,68 @@
+<#
+    .SYNOPSIS
+    Returns the piped object if a specified string property contains any partial value from a list.
+
+    .DESCRIPTION
+    The Get-ObjectWithMatchingProperty function takes in a list (SourceList) and a string property (Property) from a piped object,
+    and checks if any part of the items in SourceList is contained within the string property. 
+    If at least one partial match is found, it returns the piped object, otherwise, it returns $null.
+
+    .PARAMETER SourceList
+    Specifies the list of values that will be searched for within the string property.
+
+    .PARAMETER Property
+    Specifies the property name in the piped object where the search will be performed.
+
+    .INPUTS
+    PSCustomObject. You can pipe objects to Get-ObjectWithMatchingProperty.
+
+    .OUTPUTS
+    PSCustomObject. Get-ObjectWithMatchingProperty returns the piped object if a match is found.
+
+    .EXAMPLE
+    PS> $obj = [PSCustomObject]@{ Name = "apple" }
+    PS> $obj | Get-ObjectWithMatchingProperty -SourceList @("app", "cher") -Property "Name"
+    @{Name='apple'}
+
+    .EXAMPLE
+    PS> $obj = [PSCustomObject]@{ Name = "apple" }
+    PS> $obj | Get-ObjectWithMatchingProperty -SourceList @("ora", "lem") -Property "Name"
+    $null
+
+    .LINK
+    Online version: [Your link here if available]
+#>
+
+function Get-ObjectWithMatchingProperty {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [array]$SourceList,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Property,
+
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [PSCustomObject]$InputObject
+    )
+
+    begin {
+        Write-Verbose "Starting Get-ObjectWithMatchingProperty function."
+    }
+
+    process {
+        $Value = $InputObject.$Property
+        foreach ($item in $SourceList) {
+            if ($Value -like "*$item*") {
+                Write-Verbose "Match found. Returning object."
+                return $InputObject
+            }
+        }
+        Write-Verbose "No match found. Returning null."
+        return $null
+    }
+
+    end {
+        Write-Verbose "Ending Get-ObjectWithMatchingProperty function."
+    }
+}
